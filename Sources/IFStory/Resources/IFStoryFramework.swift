8f0a0d6748fa7637edeee60ac70ab284b35c1933
MD SAZID HASAN DIP <dipcse07@gmail.com>
2021-08-20 00:26:54 +0600

    commit for package manager

---
+++ Sources/IFStory/Resources/IFStoryFramework.swift
@@ -0,0 +1,38 @@
+//
+//  File.swift
+//  
+//
+//  Created by MD SAZID HASAN DIP on 20/8/21.
+//
+
+import Foundation
+
+public class IFStoryFramework : NSObject {
+    static let bundleName = "IFStory"
+
+    public static let bundle = Bundle(for: Self.self)!
+
+    /// Returns the resource bundle associated with the current Swift module. This is required for SPM use
+    public static var module: Bundle = {
+        let bundleName = "IFStory" // May be "MyFramework_MyFramework" for you
+
+        let candidates = [
+            // Bundle should be present here when the package is linked into an App.
+            Bundle.main.resourceURL,
+
+            // Bundle should be present here when the package is linked into a framework.
+            Bundle(for: MyFramework.self).resourceURL,
+
+            // For command-line tools.
+            Bundle.main.bundleURL
+        ]
+
+        for candidate in candidates {
+            let bundlePath = candidate?.appendingPathComponent(bundleName + ".bundle")
+            if let bundle = bundlePath.flatMap(Bundle.init(url:)) {
+                return bundle
+            }
+        }
+        return MyFramework.bundle
+    }()
+}
